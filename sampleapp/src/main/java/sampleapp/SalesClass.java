8bb509ec5f243b36830c5ac88599d56f5dc48ae9
arypatil <arypatil@LIN25005225.corp.capgemini.com>
2021-08-17 11:06:16 +0530

    display method updated in sales class

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -4,8 +4,10 @@ public class SalesClass {
 
 	public void display() {
 		int[] arr= {1,2,3,4,5};
-		for(int num:arr)
-			System.out.println(num);
+		for(int i=0;i<arr.length,i++)
+			System.out.println(arr[i]);
+				
+		System.out.println("Array size:" + arr.length);
 	}
 	public void print() {
 		System.out.println("Array numbers:");
